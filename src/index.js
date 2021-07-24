35338afdc026df885a8c6000fcb5b8b7c2f301ed
Everton <evertonpinheiroti@gmail.com>
2021-07-23 21:29:39 -0300

    :shirt: Created LINT config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (request, response) => {
   response.send('Hello World!');
-})
+});
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
