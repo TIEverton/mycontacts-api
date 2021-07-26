87d49710ac07b221a347a6c4ba6a6af4dd9e181b
Everton <evertonpinheiroti@gmail.com>
2021-07-26 14:36:32 -0300

    :sparkles: Created contact registration route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
-const routes = require('./routes')
+const routes = require('./routes');
 
 const app = express();
-app.use(routes)
+
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
