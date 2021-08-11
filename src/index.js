7858d28c7ac0961a680446b241e3008c8d183055
Everton <evertonpinheiroti@gmail.com>
2021-08-11 11:30:42 -0300

    :hammer: Refactoring code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+require('express-async-errors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -7,5 +9,9 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
